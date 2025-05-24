fb6e33a5d91fa60323ad58b93eecb263628f0170
alexis <mailalesci@gmail.com>
2025-05-24 17:41:12 +0200

    FEAT: add debug toolbar and optimize index loading

--- core/views.py
+++ core/views.py
@@ -12,7 +12,7 @@ from moderation.services import moderate_text
 
 # Create your views here.
 def index(request):
-    all_offers = Offer.objects.filter(filled=False).filter(Q(moderation__isnull=True) | Q(moderation__passed=True)).order_by('-created_on')
+    all_offers = Offer.objects.select_related('moderation').filter(filled=False).filter(Q(moderation__isnull=True) | Q(moderation__passed=True)).order_by('-created_on')
     return render(request, 'core/index.html', {'all_offers': all_offers})
 
 
